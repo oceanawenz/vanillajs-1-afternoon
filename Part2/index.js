032944704d30cfa19d1ff4c6a3bbf6b40d9b7824
Oceana Wenz <oceana.wenz@gmail.com>
2019-04-29 16:43:55 -0700

    halfway finished

---
+++ Part2/index.js
@@ -0,0 +1,9 @@
+const idInput = document.getElementById("idInput");
+const colorInput = document.getElementById("colorInput");
+console.log(idInput);
+console.log(colorInput);
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  console.log(card);
+}
